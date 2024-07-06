8add425f3e216fa599c00b0a0c79a4791361263a
Ignacio Alonso <ignacio.alley@gmail.com>
2024-07-06 14:15:15 -0600

    Update App component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import BrokerSimulator from './BrokerSimulator.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-gray-100 min-h-screen py-8">
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-bold text-center mb-8">Simulador de Comparación de Brokers</h1>
+        <BrokerSimulator />
+      </div>
     </div>
   );
 }
